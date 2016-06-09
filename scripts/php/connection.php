7578eb5af341d6026f4170eb672b7fd0afd7f27a
bpeterson1663 <bpeterson1663@gmail.com>
2016-06-09 11:39:27 -0500

    DB Update for Heroku

--- scripts/php/connection.php
+++ scripts/php/connection.php
@@ -1,8 +1,15 @@
 <?php
- $host = 'localhost';
- $user = "root";
- $password = "root";
- $db = "winePad";
- $link = mysqli_connect($host, $user, $password, $db);
+ // $host = 'localhost';
+ // $user = "root";
+ // $password = "root";
+ // $db = "winePad";
+ // $link = mysqli_connect($host, $user, $password, $db);
+
+ $server = 'us-cdbr-iron-east-04.cleardb.net';
+ $username = 'bc2c2a7b9fa38a';
+ $password = 'c529e739';
+ $db = 'heroku_d318c8835a649ae';
+
+ $link = mysqli_connect($server, $username, $password, $db);
 
  ?>
