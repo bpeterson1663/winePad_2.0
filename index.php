168c43463de682d939f8a4802c6379dff08df1bc
bpeterson1663 <bpeterson1663@gmail.com>
2016-09-26 23:02:42 -0500

    Removed jQuery and Bootstrap

--- index.php
+++ index.php
@@ -118,67 +118,60 @@
     <link href="vendors/angular-material/angular-material.min.css" type="text/javascript"></script>
     <link href="styles/styles.css" rel="stylesheet" />
   </head>
-<body data-spy="scroll" data-target=".navbar-collapse">
-  <div class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-      <div class="navbar-header">
-        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
+<body>
+  <div>
+    <div>
+      <div >
+        <button>
+            <span></span>
+            <span></span>
+            <span></span>
         </button>
-        <a class="navbar-brand">winePad</a>
+        <a>winePad</a>
       </div>
-      <div class="collapse navbar-collapse">
-        <form class="navbar-form navbar-right" method="POST">
-          <div class="form-group">
+      <div>
+        <form method="POST">
+        
             <label for="loginusername">User Name: </label>
-            <input class="form-control" placeholder="User name" type="text" name="loginusername" id="loginusername" />
-          </div>
-          <div class="form-group">
+            <input placeholder="User name" type="text" name="loginusername" id="loginusername" />
+  
             <label for="loginpassword">Password: </label>
-            <input class="form-control" placeholder="Password" type="password" name="loginpassword" />
+            <input placeholder="Password" type="password" name="loginpassword" />
           </div>
 
-          <input class="btn btn-danger" type="submit" name="submit" value="Log In" />
+          <inputtype="submit" name="submit" value="Log In" />
         </form>
       </div>
     </div>
   </div>
 
-  <div class="container" id="topContainer">
-    <div class="row">
-      <div class="col-md-6 col-md-offset-3">
+  <div id="topContainer">
+    <div>
+      <div>
         <h1>winePad</h1>
-        <p class="lead">Your Personal Wine Cellar Management System</p>
+        <p>Your Personal Wine Cellar Management System</p>
         <?php
         //Display any eorrs
           if($error){
-            echo '<div class="alert alert-danger">'.addslashes($error).'</div>'; //.addslashes escapes any quotes
+            echo '<div>'.addslashes($error).'</div>'; //.addslashes escapes any quotes
           }
           if($message){
-            echo '<div class="alert alert-success">'.addslashes($message).'</div>';
+            echo '<div>'.addslashes($message).'</div>';
           }
          ?>
         <form method="POST">
-          <div class="form-group">
             <label for="email">Name</label>
-            <input class="form-control" placeholder="Name" type="text" name="name" id="name" value="<?php echo addslashes($_POST['name']);?>"/>
-          </div>
-          <div class="form-group">
-            <label for="email">User Name</label>
-            <input class="form-control" placeholder="User Name" type="text" name="username" id="username" value="<?php echo addslashes($_POST['username']);?>"/>
-          </div>
-          <div class="form-group">
-            <label for="email">Email</label>
-            <input class="form-control" placeholder="Email" type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
-          </div>
-          <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" placeholder="Password" type="password" name="password" value="<?php echo addslashes($_POST['password']);?>"/>
-          </div>
+            <input placeholder="Name" type="text" name="name" id="name" value="<?php echo addslashes($_POST['name']);?>"/>
 
-          <input class="btn btn-lg btn-danger" type="submit" name="submit" value="Register" />
+            <label for="email">User Name</label>
+            <inputplaceholder="User Name" type="text" name="username" id="username" value="<?php echo addslashes($_POST['username']);?>"/>
+ 
+            <label for="email">Email</label>
+            <input placeholder="Email" type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
+
+            <label for="password">Password</label>
+            <input placeholder="Password" type="password" name="password" value="<?php echo addslashes($_POST['password']);?>"/>
+          <input  type="submit" name="submit" value="Register" />
         </form>
       </div>
     </div>
